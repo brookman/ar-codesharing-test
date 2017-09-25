1aad00f3ad3c350fe7a92adaaacd79e4c004ea7e
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2017-09-25 12:48:24 +0200

    Add dummy unit test.

---
+++ Shared/Assets/Content/Scripts/Editor/TestScriptTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[TestFixture]
+public class TestScriptTest {
+
+    [Test]
+    public void DummyTestMethod_RunUnitTests_ShouldPass()
+    {
+  
+        Assert.True(true);
+    }
+}
