fa96bf10c18817dc3917b5223966609043b7971a
Benjamin Bachmann <benjamin.bachmann@zuehlke.com>
2017-09-25 12:11:08 +0200

    Re-organize folder structure, add example asset.

--- create_symlinks_windows.bat
+++ create_symlinks_windows.bat
@@ -1,3 +1,24 @@
-cd android\AndroidProject\Assets
-rmdir Shared
-mklink /D Shared ..\..\..\shared\Shared\Assets
+cd Android\Assets
+rmdir SharedContent
+mklink /D SharedContent ..\..\Shared\Assets\Content
+cd ..\..
+
+cd Android\Assets\Plugins
+rmdir AssetStore
+mklink /D AssetStore ..\..\..\AssetStore\Assets
+cd ..\..\..
+
+cd IOS\Assets
+rmdir SharedContent
+mklink /D SharedContent ..\..\Shared\Assets\Content
+cd ..\..
+
+cd IOS\Assets\Plugins
+rmdir AssetStore
+mklink /D AssetStore ..\..\..\AssetStore\Assets
+cd ..\..\..
+
+cd Shared\Assets\Plugins
+rmdir AssetStore
+mklink /D AssetStore ..\..\..\AssetStore\Assets
+cd ..\..\..
